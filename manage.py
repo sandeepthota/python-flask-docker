e3a2ec61a82c328122635986c45f7dcd0fe57b99
sandeepthota <sandeepthota.nitj@gmail.com>
2017-12-23 19:56:43 -0500

    First commit - flask setup and basic ping

---
+++ manage.py
@@ -0,0 +1,14 @@
+# manage.py
+
+
+from flask_script import Manager
+
+from project import app
+
+
+manager = Manager(app)
+
+
+if __name__ == '__main__':
+    manager.run()
+
---
+++ project/config.py
@@ -0,0 +1,24 @@
+# project/config.py
+
+
+class BaseConfig:
+    """Base configuration"""
+    DEBUG = False
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    DEBUG = True
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    DEBUG = True
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    DEBUG = False
+
